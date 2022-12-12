093be5f0df1ae747b82666f4a2f6e045528b852c
rdonni <rayane.donni@imt-atlantique.net>
2022-12-13 00:12:12 +0100

    Factorize, add comments and typing

--- collect_tweets.py
+++ collect_tweets.py
@@ -1,6 +1,5 @@
 # Imports
 import pickle
-from time import sleep
 
 import tweepy
 from langdetect import detect
@@ -12,143 +11,136 @@ from tools import LANGUAGES
 
 # Collect tweets
 def collect_tweets(api,
-                   geocodes,
-                   tweet_file_path,
-                   keyword = '*',
-                   nb_tweets_per_country = 5,
-                   clear = False, 
-                   ) : 
-        
-    # Handle the tweet collected previously
-    # If clear = True, it clears the file tweets_by_countries.py. The variable tweets_by_countries is set to {}
-    # If clear = False, it loads the previously collected tweets in the variable tweets_by_countries
+                   geocodes: dict[str, str],
+                   tweet_file_path: str,
+                   keyword: str = '*',
+                   nb_tweets_per_country: int = 5,
+                   clear: bool = True,
+                   ):
+    """
+    Return and save a dictionnary containing tweets by country 
+
+    The clear parameter handle the tweet collected previously:
+    - if clear = True, it clears the file tweets_by_countries.py containing the tweets.
+    The variable tweets_by_countries is set to {}
+    - if clear = False, it loads the previously collected tweets in the variable tweets_by_countries
+    """
 
     # Load tweet files if necessary
     tweets_by_countries = load_tweet_files(clear, tweet_file_path)
-    
+
     # Collect new tweets
-    for country in tqdm(geocodes.keys()): 
-        print (country)
-        
+    for country in (pbar := tqdm(geocodes.keys())):
+        pbar.set_description(f"Processing {country}")
+
         # Translate the keyword that (given in english) into each country's language
         new_keyword = generate_translated_keyword(keyword, country)
-        
+
         tweets = tweepy.Cursor(api.search_tweets,
-                               q = new_keyword,
-                               geocode = geocodes[country],
-                               tweet_mode = "extended",
-                               count = 100
+                               q=new_keyword,
+                               geocode=geocodes[country],
+                               tweet_mode="extended",
+                               count=100
                                ).items(nb_tweets_per_country)
-                
-        #Collect tweets
+
+        # Collect tweets
         data_country = translate_and_save_tweets(tweets,
                                                  country)
-        
-        update_and_save_tweet_file(tweets_by_countries, 
+
+        update_and_save_tweet_file(tweets_by_countries,
                                    country,
                                    data_country,
                                    tweet_file_path)
-                                        
-    print ("----------------------- tweet collected -----------------------")
+
+    print("----------------------- tweet collected -----------------------")
     return tweets_by_countries
 
-def translate_keyword_from_en_to_langage(string,
-                                 langage):
+
+def translate_keyword_from_en_to_langage(string: str, langage: str) -> str:
     if langage == 'en':
         return string
-    else :
-        return str(TextBlob(string).translate(from_lang = 'en', to = langage))
+    else:
+        return str(TextBlob(string).translate(from_lang='en', to=langage))
 
-def translate_tweet_from_langage_to_en(tweet_text,
-                                       langage):
-    if detect(tweet_text) =='en':
+
+def translate_tweet_from_langage_to_en(tweet_text: str,langage: str) -> str:
+    if detect(tweet_text) == 'en':
         return tweet_text
-    else :
-        return str(TextBlob(tweet_text).translate(from_lang = langage, to = 'en'))
-    
+    else:
+        return str(TextBlob(tweet_text).translate(from_lang=langage, to='en'))
 
-def generate_translated_keyword(keyword: str,
-                                country : str) -> str:
-    
-    if keyword =='*':
+
+def generate_translated_keyword(keyword: str, country: str) -> str:
+    if keyword == '*':
         return keyword
-    else :
+    else:
         spoken_langages = eval(LANGUAGES[country])
-        
-        if (len(spoken_langages) == 1) :
+
+        if (len(spoken_langages) == 1):
             return keyword
-            
-        else :
+        else:
             new_keyword = ''
             for i in range(len(spoken_langages)):
-                new_keyword += translate_keyword_from_en_to_langage(keyword, spoken_langages[i])   
+                new_keyword += translate_keyword_from_en_to_langage(keyword, spoken_langages[i])
                 if i != len(spoken_langages)-1:
                     new_keyword += ' OR '
             return new_keyword
-                    
 
-def translate_and_save_tweets(tweets,
-                              country):
-    
-    data_country=[]
-    
-    for tweet in tqdm(tweets) : 
 
+def translate_and_save_tweets(tweets, country: str) -> list[str, str, str, str]:
+
+    data_country = []
+    for tweet in tqdm(tweets):
         tweet_text = tweet.full_text
-
         for langage in eval(LANGUAGES[country]):
-            try :
-                tweet_text = translate_tweet_from_langage_to_en(tweet_text, langage) 
+            try:
+                tweet_text = translate_tweet_from_langage_to_en(
+                    tweet_text, langage)
                 data_country.append([country, tweet.created_at, tweet.user.screen_name, str(tweet_text)])
                 continue
-            except :
+            except:
                 pass
-                
-        #sleep(0.5)
-    return data_country
-            
 
-def load_tweet_files(clear,
-                     tweet_file_path):
-    
-    if clear == True : 
+        # sleep(0.5)
+    return data_country
+
+
+def load_tweet_files(clear: bool, tweet_file_path: str):
+    if clear == True:
         tweets_by_countries = {}
-        tweet_file = open(tweet_file_path, "wb") #We open the file containing the tweets
-        tweet_file.truncate(0) #Clear the file
+        # We open the file containing the tweets
+        tweet_file = open(tweet_file_path, "wb")
+        tweet_file.truncate(0)  # Clear the file
         pickle.dump(tweets_by_countries, tweet_file)
         tweet_file.close()
-    else : 
-        try : # Try to load the tweet dictionnary if it already exists
-            with open(tweet_file_path, "rb") as handle :
+    else:
+        try:  # Try to load the tweet dictionnary if it already exists
+            with open(tweet_file_path, "rb") as handle:
                 data = handle.read()
             tweets_by_countries = pickle.loads(data)
-        except : # If it doesn't exist, create an empty dictionnary and save it into a specific directory
+        except:  # If it doesn't exist, create an empty dictionnary and save it into a specific directory
             tweets_by_countries = {}
             tweet_file = open(tweet_file_path, "wb")
             pickle.dump(tweets_by_countries, tweet_file)
             tweet_file.close()
-            
+
     return tweets_by_countries
 
-def update_and_save_tweet_file(tweets_by_countries, 
+
+def update_and_save_tweet_file(tweets_by_countries,
                                country,
                                data_country,
                                tweet_file_path
-                                ):
-    
+                               ):
+
     # Update the tweets_by_countries dictionnary
-    if tweets_by_countries.get(country) : # Check if the value of the key country is not empty
-        old_tweets = tweets_by_countries[country]
-        updated_tweets = old_tweets + data_country
-        tweets_by_countries[country] = updated_tweets
-        
-    else : 
-        tweets_by_countries[country] = data_country               
-            
+    # Check if the value of the key country is not empty
+    if tweets_by_countries.get(country):
+        tweets_by_countries[country] = tweets_by_countries[country] + data_country
+    else:
+        tweets_by_countries[country] = data_country
+
     # Save the updated dictionnary in its specific path
     tweet_file = open(tweet_file_path, "wb")
     pickle.dump(tweets_by_countries, tweet_file)
     tweet_file.close()
-    
-
-
--- update_status.py
+++ update_status.py
@@ -1,35 +1,15 @@
-import tweepy 
-import configparser
-import cv2
+from datetime import date
 
-# read config and authentification
-config = configparser.ConfigParser()
-config.read('/Users/rayanedonni/Documents/Projets_persos/News_by_ai/config.ini')
 
-api_key = config['twitter']['api_key']
-api_key_secret = config['twitter']['api_key_secret']
-access_token = config['twitter']['access_token']
-access_token_secret = config['twitter']['access_token_secret']
+def todays_date():
+    today = date.today()
+    return today.strftime("%B %d, %Y")
 
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+def default_tweet_text(keyword):
+    return f"Sentiment map of {todays_date()} with keyword : {keyword}"
 
-tweet_text='Map of European sentiment of 25th May'
-image_path ='/Users/rayanedonni/Documents/Projets_persos/News_by_ai/sentiment_analysis/sentiment_map.png'
+def upload_tweet(api, tweet_text: str, image_path: str) -> None:
+    upload = api.media_upload(filename = image_path)
+    media_ids = [upload.media_id_string]
+    api.update_status(media_ids = media_ids, status=tweet_text)
 
-im = cv2.imread(image_path)# Read image
-
-new_width = int(im.shape[1]*0.5)
-new_height = int(im.shape[0]*0.5)
-dim = (new_width, new_height)
-
-imS = cv2.resize(im, dim)  
-cv2.imwrite('/Users/rayanedonni/Documents/Projets_persos/News_by_ai/sentiment_analysis/sentiment_map_resized.png', imS)
-
-image_resized_path = '/Users/rayanedonni/Documents/Projets_persos/News_by_ai/sentiment_analysis/sentiment_map_resized.png'
-
-#Generate text tweet with media (image)
-upload = api.media_upload(filename = image_resized_path)
-media_ids = [upload.media_id_string]
-result = api.update_status(media_ids = media_ids, status=tweet_text)
