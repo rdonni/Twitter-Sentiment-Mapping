7ef62c986590163e4ae9263a732b8538160c2352
rdonni <rayane.donni@imt-atlantique.net>
2022-12-12 22:57:36 +0100

    Add comments to script geocodes.py

--- geocodes.py
+++ geocodes.py
@@ -1,4 +1,4 @@
-#Imports
+# Imports
 from time import sleep
 
 import numpy as np
@@ -6,15 +6,11 @@ import requests
 from geopy import distance
 from tqdm import tqdm
 
-from tools import COUNTRIES
 
-
-# Get the geocode of each country : geocode:latitude,longitude,radius --> example : geocode:40.712776,-74.005974,10km
-# We use nominatim APIs which is the geocoding software that powers Open Street Map
-#https://gis.stackexchange.com/questions/212796/get-lat-lon-extent-of-country-from-name-using-python
-def get_boundingbox_country(country, output_as='boundingbox'):
+def get_boundingbox_country(country, output_as='boundingbox') -> list[float, float]:
     """
-    get the bounding box of a country in EPSG4326 given a country name
+    Get the bounding box of a country in EPSG4326 given a country name
+    Value are given by nominatim APIs which is the geocoding software that powers Open Street Map
 
     Parameters
     ----------
@@ -30,57 +26,61 @@ def get_boundingbox_country(country, output_as='boundingbox'):
     output : list
         list with coordinates as str
     """
-    # create url
+    # Create url
     url = 'http://nominatim.openstreetmap.org/search?country=' + country + '&format=json&polygon=0'
     response = requests.get(url).json()[0]
 
-    # parse response to list
+    # Parse response to list
     if output_as == 'boundingbox':
         lst = response[output_as]
         output = [float(i) for i in lst]
     if output_as == 'center':
-        lst = [response.get(key) for key in ['lat','lon']]
+        lst = [response.get(key) for key in ['lat', 'lon']]
         output = [float(i) for i in lst]
     return output
 
 
-def minimal_radius(country) :
+def minimal_radius(country: str) -> float:
     """
-    get the minimal radius for a circle to encompass a country
-    
+    Return the minimal radius for a circle to encompass a country as a float
     """
 
-    latmin, latmax, lonmin, lonmax = np.array(get_boundingbox_country(country, output_as = 'boundingbox'))
-    center = np.array(get_boundingbox_country(country, output_as = 'center'))
-    
-    # Point that we will calculate the distance to the center to define the smallest radius that encompasses the country
-    extremal_points = [np.array([center[0], lonmin]), np.array([center[0], lonmax]), np.array([latmin, center[1]]), np.array([latmax, center[1]])]
-    
+    latmin, latmax, lonmin, lonmax = np.array(get_boundingbox_country(country, output_as='boundingbox'))
+    center = np.array(get_boundingbox_country(country, output_as='center'))
+
+    # We calculate the distance between these points and the center of the country 
+    # to define the smallest radius that encompasses the country
+    extremal_points = [np.array([center[0], lonmin]),
+                       np.array([center[0], lonmax]),
+                       np.array([latmin, center[1]]),
+                       np.array([latmax, center[1]])]
+
     # Distance between these extremal points to the center
-    possible_radiuses = []    
-    for point in extremal_points : 
+    possible_radiuses = []
+    for point in extremal_points:
         possible_radiuses.append(distance.geodesic(point, center).km)
-        
+
     radius = min(possible_radiuses)
 
     return radius
 
-def geocode(countries) : 
-    """
-    generate a dictionnary of the following form {country : geocode} from a list of countries
-    
-    """
-    
-    geocode = {}
-    for country in tqdm(countries) :
-        if country == "Ukraine" :
-            geocode[country] = "48.2289622,27.1482283,200km"
-        
-        else :
-            country_center = get_boundingbox_country(country, output_as='center')
-            geocode_country = f'{country_center[0]},{country_center[1]},{minimal_radius(country)}km'
-            geocode[country] = geocode_country
-            sleep (0.5)
-    print ("----------------------- geocodes generated -----------------------")
-    return geocode
 
+def geocode(countries: list[str]) -> dict[str, str]:
+    """
+    Generate a dictionnary dict[country: str, geocode: str] from a list of countries
+
+    For some countries not available in nominatim APIs (as Ukraine)
+    an approximation of the coordinates obtained by hand is provided
+    """
+
+    geocode = {}
+    for country in tqdm(countries):
+        if country == "Ukraine":
+            geocode[country] = "48.2289622,27.1482283,200km"
+        else:
+            country_center = get_boundingbox_country(country, output_as='center')
+            geocode[country] = f'{country_center[0]},{country_center[1]},{minimal_radius(country)}km'
+
+        sleep(0.5)
+    print("----------------------- geocodes generated -----------------------")
+    return geocode
