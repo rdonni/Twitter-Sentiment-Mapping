8740b220f356bc65234bc0cbb958fd30a6c046d6
rdonni <rayane.donni@imt-atlantique.net>
2022-12-14 00:06:03 +0100

    Refactor

--- main.py
+++ main.py
@@ -11,48 +11,47 @@ from vizualisation.vizualisation import generate_map
 from update_status import default_tweet_text, upload_tweet
 
 
-#@click.command()
-#@click.option('--config-path', default='/Users/rayanedonni/Documents/Projets_persos/News_by_ai/config.ini', required=True)
-#@click.option('--tweets-path', default='/Users/rayanedonni/Documents/Projets_persos/News_by_ai/sentiment_analysis/tweets_by_countries.pkl', required=True)
-#@click.option('--keyword', default='*')
-#@click.option('--compute-geocodes, default=True)
-#@click.option('--nb-tweets-to-collect-by-country', default=3)
-#@click.option('--clear', default=False)
-#@click.option('--map-output-path', default='Vizualisation/sentiment_map.jpg')
-#@click.option('--update-status', default=False)
+# @click.command()
+# @click.option('--config-path', default='/Users/rayanedonni/Documents/Projets_persos/News_by_ai/config.ini', required=True)
+# @click.option('--tweets-path', default='/Users/rayanedonni/Documents/Projets_persos/News_by_ai/sentiment_analysis/tweets_by_countries.pkl', required=True)
+# @click.option('--keyword', default='*')
+# @click.option('--compute-geocodes, default=True)
+# @click.option('--nb-tweets-to-collect-by-country', default=3)
+# @click.option('--clear', default=False)
+# @click.option('--map-output-path', default='Vizualisation/sentiment_map.jpg')
+# @click.option('--update-status', default=False)
 def main(
-    config_path: str='/Users/rayanedonni/Documents/Projets_persos/News_by_ai/config.ini',
-    tweets_path: str="/Users/rayanedonni/Documents/Projets_persos/News_by_ai/sentiment_analysis/tweets_by_countries.pkl",
-    keyword: str='*',
-    compute_geocodes: bool= False,
-    nb_tweets_to_collect_by_country: int=5,
-    clear: bool=True,
-    map_output_path: str='vizualisation/sentiment_map.png', 
-    update_status: bool=False):
-    
-    api=get_api_access(config_path)
-    
-    if compute_geocodes == True:
+        config_path: str = '/Users/rayanedonni/Documents/Projets_persos/News_by_ai/config.ini',
+        tweets_path: str = "/Users/rayanedonni/Documents/Projets_persos/News_by_ai/sentiment_analysis/tweets_by_countries.pkl",
+        keyword: str = '*',
+        compute_geocodes: bool = False,
+        nb_tweets_to_collect_by_country: int = 5,
+        clear: bool = True,
+        map_output_path: str = 'vizualisation/sentiment_map.png',
+        update_status: bool = False) -> object:
+    api = get_api_access(config_path)
+
+    if compute_geocodes:
         geocodes = geocode(COUNTRIES)
-    else :
+    else:
         geocodes = GEOCODES
-    
+
     tweets_by_countries = collect_tweets(api, geocodes, tweets_path, keyword, nb_tweets_to_collect_by_country, clear)
-    
-    for country in tweets_by_countries.keys() :
+
+    for country in tweets_by_countries.keys():
         print(country, " :", len(tweets_by_countries[country]))
 
-    tweets_by_countries = preprocessing(tweets_by_countries)    
+    tweets_by_countries = preprocessing(tweets_by_countries)
 
     sentiment_scores_by_countries = sentiment_analysis(tweets_by_countries)
 
     negativity_score_by_countries = score(sentiment_scores_by_countries)
-    for key,value in negativity_score_by_countries.items() :
-        print (key, " :", value)
+    for key, value in negativity_score_by_countries.items():
+        print(key, " :", value)
 
     generate_map(negativity_score_by_countries, map_output_path)
 
-    if update_status == True:        
+    if update_status:
         upload_tweet(api, default_tweet_text(keyword), map_output_path)
 
 
